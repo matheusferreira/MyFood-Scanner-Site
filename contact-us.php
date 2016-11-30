d5da96e638c29a53d6be8949feed95a5a924cf2f
Matheus Ferreira Mendonça <mtsdesigner@ymail.com>
2016-11-30 18:19:12 -0200

    Envio de email atraves de sendgrid web api

--- contact-us.php
+++ contact-us.php
@@ -198,9 +198,45 @@ if(isset($_POST['email'])) {
 
     echo("<script>console.log('PHP: criação dos headers');</script>");
      
-    mail($email_to, $email_subject, $email_message, $headers);
+    // mail($email_to, $email_subject, $email_message, $headers);
 
     echo("<script>console.log('PHP: envio do mail');</script>");
+
+     $url = 'https://api.sendgrid.com/';
+     $user = 'azure_526d0671b57fa2464886e31c0c2d3b30@azure.com';
+     $pass = 'behappysmtp1'; 
+
+     $params = array(
+          'api_user' => $user,
+          'api_key' => $pass,
+          'to' => 'mts.seven@gmail.com',
+          'subject' => 'New message from MyFoodScanner',
+          'html' => 'testing body',
+          'text' => $email_message,
+          'from' => $email_from,
+       );
+
+     $request = $url.'api/mail.send.json';
+
+     // Generate curl request
+     $session = curl_init($request);
+
+     // Tell curl to use HTTP POST
+     curl_setopt ($session, CURLOPT_POST, true);
+
+     // Tell curl that this is the body of the POST
+     curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+
+     // Tell curl not to return headers, but do return the response
+     curl_setopt($session, CURLOPT_HEADER, false);
+     curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+     // obtain response
+     $response = curl_exec($session);
+     curl_close($session);
+
+     // print everything out
+     print_r($response);
 }
  
 ?>
